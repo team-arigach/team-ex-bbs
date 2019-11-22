57aa5efe0fa5bb0080fdafb0d71ecc6fea4941b9
su-san1129 <deviationvalue1129@icloud.com>
2019-11-22 11:36:04 +0900

    first commit

---
+++ src/main/java/com/example/TeamExBbsApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamExBbsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamExBbsApplication.class, args);
+	}
+
+}

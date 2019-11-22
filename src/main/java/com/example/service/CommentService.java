7cca37a3e719dbe082e596d027edb091c7ac6450
suzuki-ryouhei <ryohei.suzuki@rakus-partners.co.jp>
2019-11-22 13:50:46 +0900

    サービスを作成

---
+++ src/main/java/com/example/service/CommentService.java
@@ -0,0 +1,27 @@
+package com.example.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.domain.Comment;
+import com.example.repository.CommentRepository;
+
+import jp.co.runy.bbs.joined.domain.JoinedComment;
+import jp.co.runy.bbs.joined.repository.JoinedCommentRepository;
+
+@Service
+public class CommentService {
+	@Autowired
+	private CommentRepository commentRepository;
+
+	/**
+	 * コメントを登録します.
+	 * 
+	 * @param comment
+	 *            コメント情報
+	 * @return 登録したコメント情報
+	 */
+	public void save(Comment comment) {
+		commentRepository.insert(comment);
+	}
+}
